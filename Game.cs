1e00946bdc84b1065bbfbf5597cabdc1320acd7e
Will Curry <watercrafthd@gmail.com>
2016-09-26 15:46:11 +0100

    Add game runner

--- Game.cs
+++ Game.cs
@@ -10,6 +10,47 @@ public class Game {
         this.playerInactive = playerInactive;
     }
 
+    public static void Main(string[] args) {
+        Board board = new Board("---------");
+        Game game = new Game(board, new HumanPlayer('x'), new HumanPlayer('o'));
+        game.start();
+    }
+
+    private void start() {
+        pickGameMode();
+        while (!board.isGameOver()) {
+            displayBoard();
+            playerMakeMove();
+        }
+    }
+
+    public void displayGamemodes() {
+        Console.WriteLine("What game mode would you like play?");
+        Console.WriteLine("1) Player vs Player");
+        Console.WriteLine("2) Player vs Computer");
+        Console.WriteLine("3) Computer vs Computer");
+        Console.WriteLine("───────────────────────────────────");
+    }
+
+    public void pickGameMode() {
+        displayGamemodes();
+        int pick;
+        int.TryParse(Console.ReadLine(), out pick);
+        if (pick == 2) {
+            playerInactive = new ComputerPlayer('o');
+        } else if(pick == 3) {
+            playerActive = new ComputerPlayer('x');
+            playerInactive = new ComputerPlayer('o');
+        }
+    }
+
+    private void displayBoard() {
+        Console.WriteLine(board.board.Substring(0,3));
+        Console.WriteLine(board.board.Substring(3, 3));
+        Console.WriteLine(board.board.Substring(6, 3));
+        Console.WriteLine("───────────────────────────────────");
+    }
+
     public int playerNextMove() {
         return playerActive.nextMove(this);
     }
